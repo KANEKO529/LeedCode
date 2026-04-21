85e166b52b1bd49d18042e5935776340e15a6e61
KANEKO529 <kanekohiromasa529@gmail.com>
2026-04-21 20:05:37 +0900

    119 success

--- scripts/easy_119_pascals_triangle_ii.go
+++ scripts/easy_119_pascals_triangle_ii.go
@@ -11,6 +11,26 @@ func getRow(rowIndex int) []int {
 	// -----   my answer    -----
 	// --------------------------
 
+	results := make([][]int, 0, rowIndex)
+
+	for i := 0; i <= rowIndex; i++ {
+		// resultsに格納する配列
+		row := make([]int, i+1)
+
+		// 先頭と末尾は必ず1
+		row[0] = 1
+		row[i] = 1
+
+		// 真ん中を埋める
+		for j := 1; j < i; j++ {
+			row[j] = results[i-1][j-1] + results[i-1][j]
+		}
+
+		results = append(results, row)
+	}
+
+	return results[rowIndex]
+
 	// --------------------------
 	// ----- correct answer  ----
 	// --------------------------
