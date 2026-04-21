e427c50ac3953f260e5fa3f9e240efaa3e8d3a84
KANEKO529 <kanekohiromasa529@gmail.com>
2026-04-21 14:34:00 +0900

    112

--- scripts/easy_112_path_sum.go
+++ scripts/easy_112_path_sum.go
@@ -4,6 +4,8 @@
  * [112] Path Sum
  */
 
+package scripts
+
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -14,7 +16,79 @@
  * }
  */
 func hasPathSum(root *TreeNode, targetSum int) bool {
-    
-}
-// @lc code=end
 
+	// --------------------------
+	// -----   my answer    -----
+	// --------------------------
+
+	// targetSum になるパスがあるかどうはの判定
+	// current を回す
+	// 左から回していく
+	// 途中で目標値に一致ではなく、葉まで到達させて、判定
+
+	// flag := false
+
+	// var kansu func(current *TreeNode, target int) bool
+
+	// kansu = func(current *TreeNode, target int) bool {
+
+	// 	if current != nil {
+	// 		return false
+	// 	}
+
+	// 	if flag {
+	// 		return flag
+	// 	}
+
+	// 	if target-current.Val == 0 {
+	// 		flag = true
+	// 		return flag
+	// 	}
+
+	// 	// 左チェック
+	// 	// 空じゃなければ再帰
+	// 	if current.Left == nil {
+	// 		return kansu(current.Right, target-current.Val)
+	// 	}
+
+	// 	// 右チェック
+	// 	// 空じゃなければ再帰
+	// 	if current.Right == nil {
+	// 		return kansu(current.Left, target-current.Val)
+	// 	}
+
+	// 	return flag
+
+	// }
+
+	// return kansu(root, targetSum)
+
+	// --------------------------
+	// ----- correct answer  ----
+	// --------------------------
+
+	// 再帰 = 「関数の意味」で考える
+	// 呼び出し = 「子に仕事を投げる」
+	// 返り値 = 「子の結果をどう合成するか」
+
+	var kansu func(current *TreeNode, target int) bool
+
+	kansu = func(current *TreeNode, target int) bool {
+		if current == nil {
+			return false
+		}
+
+		// 葉ノードなら、ここで判定
+		if current.Left == nil && current.Right == nil {
+			return target-current.Val == 0
+		}
+
+		// 左右どちらかで成立すれば true
+		return kansu(current.Left, target-current.Val) ||
+			kansu(current.Right, target-current.Val)
+	}
+
+	return kansu(root, targetSum)
+}
+
+// @lc code=end
